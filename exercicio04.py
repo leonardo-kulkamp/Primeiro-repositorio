792100b7fd0634d2a10b69aeeca7ff0619a7fa2f
leonardo-kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-05-22 14:49:53 +0000

    exercicio 04.py

---
+++ exercicio04.py
@@ -0,0 +1,8 @@
+curso01 = (int(input("Digite quantas avaliaçoes teve o curso 01 ")))
+curso02 = (int(input("Digite quantas avaliaçoes teve o curso 02 ")))
+if curso01 == curso02:
+    print("Empate")
+elif curso01 > curso02 :
+    print("Curso 01 e maior")
+else:
+    print("Curso 02 e maior")
